918a21b6a8ee130491e244f8968ec0eadf1004e3
Srishti Gauraha <67550382+Srishti44-g@users.noreply.github.com>
2021-05-05 16:33:24 +0530

    Write a PHP program to compute factorial of a number using While loop.

---
+++ Virtual Lab PHP-MYSQL/Experiment 3.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>  
+<head>  
+<title>Factorial Program using loop in PHP</title>  
+</head>  
+<body>  
+<form method="post">  
+    Enter the Number:<br>  
+    <input type="number" name="number" id="number">  
+    <input type="submit" name="submit" value="Submit" />  
+</form>  
+<?php   
+    if($_POST){  
+        $fact = 1;    
+        $number = $_POST['number'];  
+        echo "Factorial of $number:<br><br>";    
+            while($number>=1){         
+            $fact = $fact * $number;
+            $number--;  
+            }  
+            echo $fact . "<br>";  
+    }  
+?>  
+</body>  
+</html>  
