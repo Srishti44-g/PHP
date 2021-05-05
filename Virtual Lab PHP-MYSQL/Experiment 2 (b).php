8ead26e59c779c208a1ffe943e7ce446c1d5d3ee
Srishti Gauraha <67550382+Srishti44-g@users.noreply.github.com>
2021-05-05 16:32:19 +0530

    Write a PHP program Create a switch statement that  will output "Hello" if $var is "1", and "welcome" if $var is "2".

---
+++ Virtual Lab PHP-MYSQL/Experiment 2 (b).php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Using switch-case</title>
+</head>
+<body>
+<form method="post">
+  Enter the  number:<br>
+  <input type="text" name="number">
+  <input type="submit" name="submit">
+</form>
+<?php
+ if($_POST){
+     $var = $_POST['number'];
+switch($var)
+{
+    case 1 : echo "Hello";
+    break;
+    case 2 : echo "welcome";
+    break;
+}
+ }
+?>
+</body>
+</html>
