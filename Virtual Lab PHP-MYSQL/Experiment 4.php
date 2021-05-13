53114d074c49e308a44b592c676b9f2413851676
Srishti Gauraha <67550382+Srishti44-g@users.noreply.github.com>
2021-05-13 15:59:55 +0530

    Update Experiment 4.php

--- Virtual Lab PHP-MYSQL/Experiment 4.php
+++ Virtual Lab PHP-MYSQL/Experiment 4.php
@@ -1,1 +1,21 @@
+<?php
+$Num = 'a';
+$count_positive = 0;
+$count_negative = 0;
+$count_zeroes = 0;
+do{
+    $num = readline("Enter a number :");
+    if($num >0)
+      $count_positive++;
+    else if($num<0)
+      $count_negative++;
+    else
+    $count_zeroes++;
+    $Num = readline("Do you want to Continue(y/n)?");
 
+}while($Num =='a');
+echo "\n Total Positive numbers entered :", $count_positive ;
+echo "\n Total negative numbers entered :", $count_negative ; 
+echo "\n Total Zeroes entered :", $count_zeroes;
+
+?>
