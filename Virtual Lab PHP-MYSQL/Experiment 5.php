31179864652edde138628618f901b55c98dd984d
Srishti Gauraha <67550382+Srishti44-g@users.noreply.github.com>
2021-06-24 20:28:25 +0530

    Update Experiment 5.php
    Write a program that will accept an array of integers as input, and output an array 
    where for each item in the source array, the new array will perform the following 
    operations:
     a) For even numbers divide by 2
     b) For odd numbers multiply by 3

--- Virtual Lab PHP-MYSQL/Experiment 5.php
+++ Virtual Lab PHP-MYSQL/Experiment 5.php
@@ -1,1 +1,21 @@
-
+<?php
+$array = array( 1, 2, 3, 4, 5, 6, 7);
+$even_array = [];
+$odd_array = [];
+for( $i = 0 ; $i < count($array); $i++){
+ if ($array[$i] % 2 == 0){
+ array_push($even_array, $array[$i] / 2); 
+ $array[$i] = $array[$i] / 2;
+ }
+ else{
+ array_push($odd_array, $array[$i] * 3); 
+ $array[$i] = $array[$i] * 3;
+ }
+ } 
+echo "\n";
+print_r ($even_array);
+echo "\n";
+print_r ($odd_array) ;
+echo "\n new Array \n";
+print_r($array);
+?>
